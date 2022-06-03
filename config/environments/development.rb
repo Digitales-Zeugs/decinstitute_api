39454f8200a09293171b683ecd116d7c007926f4
Matias Caparros <55497544+caparrosmatias@users.noreply.github.com>
2022-06-03 13:32:58 -0300

    :bug: Hosts
    Remove dev host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  config.hosts << "0d07-190-211-90-8.sa.ngrok.io"
+  # config.hosts << "0d07-190-211-90-8.sa.ngrok.io"
   
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
